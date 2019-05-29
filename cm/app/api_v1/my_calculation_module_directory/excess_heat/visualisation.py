6a1e0b14cc214d2d642f717e7a48a75de18eb375
dav22297 <david.schilling.97@gmail.com>
2019-05-29 16:01:40 +0200

    round shp file output values

--- cm/app/api_v1/my_calculation_module_directory/excess_heat/visualisation.py
+++ cm/app/api_v1/my_calculation_module_directory/excess_heat/visualisation.py
@@ -1,6 +1,7 @@
 import fiona
 from fiona.crs import from_epsg
 from collections import OrderedDict
+from math import floor, log10
 
 output_driver = "ESRI Shapefile"
 schema = {
@@ -13,6 +14,8 @@ schema = {
                 ])
                 }
 
+round_to_n = lambda x, n: round(x, -int(floor(log10(x))) + (n - 1))
+
 
 def create_transmission_line_shp(transmission_lines, flows, temperatures, costs, lengths, file):
     with fiona.open(file,  "w", crs=from_epsg(4326), driver=output_driver, schema=schema) as shp:
@@ -23,10 +26,10 @@ def create_transmission_line_shp(transmission_lines, flows, temperatures, costs,
                     "coordinates": transmission_line
                 },
                 "properties": OrderedDict([
-                    ("Flow", str(flow) + " MWh/a"),
-                    ("Temp", str(temperature) + " C"),
-                    ("Cost", str(cost) + " Euro"),
-                    ("Length", str(length) + " km")
+                    ("Flow", str(round_to_n(flow, 3)) + " MWh/a"),
+                    ("Temp", str(round_to_n(temperature, 3)) + " C"),
+                    ("Cost", str(round_to_n(cost, 3)) + " Euro"),
+                    ("Length", str(round_to_n(length, 3)) + " km")
                 ])
             }
             shp.write(line)
