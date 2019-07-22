93ca4ac32302e13827e48c6fb045a4b3bacd8df8
David Schilling <david.schilling.97@gmail.com>
2019-07-22 14:21:45 +0200

    improve graphics

--- cm/app/api_v1/my_calculation_module_directory/run_cm.py
+++ cm/app/api_v1/my_calculation_module_directory/run_cm.py
@@ -99,7 +99,7 @@ def main(heat_density_map, pix_threshold, DH_threshold, output_raster1, output_r
                     "data": thresholds_y,
                     "pointRadius": threshold_radius,
                     "borderColor": "#fe7c60",
-                    "ShowLine": 0
+                    "showLine": False
                 },
                 {
                     "label": "Cost of network in Euros",
@@ -120,13 +120,13 @@ def main(heat_density_map, pix_threshold, DH_threshold, output_raster1, output_r
                     "data": thresholds_y2,
                     "pointRadius": threshold_radius,
                     "borderColor": "#fe7c60",
-                    "ShowLine": 0
+                    "showLine": False
                 }, {
                     "label": "Set transmission line threshold",
                     "data": thresholds_y3,
                     "pointRadius": threshold_radius,
                     "borderColor": "#fe7c60",
-                    "ShowLine": 0
+                    "showLine": False
                 },
                 {
                     "label": "levelized cost",
