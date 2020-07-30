2e5dd97df8e8194f986e5189b82d9175561c996d
fallahnejad <fallahnejad@eeg.tuwien.ac.at>
2020-07-30 12:35:56 +0200

    remove rtree to requirement

--- cm/app/api_v1/my_calculation_module_directory/excess_heat/read_data.py
+++ cm/app/api_v1/my_calculation_module_directory/excess_heat/read_data.py
@@ -370,8 +370,7 @@ def join_point_to_nuts2(industrial_database_excess_heat, path_nuts, delimiter=',
     df_industry = df_industry.dropna(subset=['geometry_wkt'])
     #df_industry [['SRID','LATLONG']] = df_industry.geom.str.split(";", expand=True,)
     gdf_industry = gpd.GeoDataFrame( df_industry, geometry=[loads_wkt(x) for x in df_industry['geometry_wkt']], crs='EPSG:4326')
-    gdf = gpd.sjoin(gdf_nuts, gdf_industry, how='right', op='intersects', lsuffix='left', rsuffix='right')
-    return gdf
+    return gpd.sjoin(gdf_nuts, gdf_industry, how='right', op='intersects', lsuffix='left', rsuffix='right')
 
 
 def ad_industrial_database_local(industrial_database_excess_heat, nuts2_ids): # here we need to get the industry sites
